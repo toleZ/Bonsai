3b936f3ee76f1b2b2c59f6b9af76f6be19b25707
Juan C. Toloy <toloyjc@gmail.com>
2023-07-13 17:33:53 -0300

    improvement: adding navbar for xs, sm and md sizes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import NavbarSM from "./NavbarSM";
+
+const Navbar = () => {
+  return (
+    <>
+      <NavbarSM />
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/NavbarSM.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const NavbarSM = () => {
+  const [isHidden, setIsHidden] = useState(true);
+
+  const handleHidde = () => setIsHidden(!isHidden);
+
+  return (
+    <nav className="fixed lg:hidden w-full flex items-center justify-between py-4 px-12">
+      <img src="/Images/logo.svg" alt="Bonsai logo" className="w-36" />
+      <button onClick={handleHidde} className="z-10">
+        {isHidden ? "🍔" : "🍅"}
+      </button>
+
+      <div
+        className={`absolute top-0 right-0 w-screen h-screen py-4 px-16 bg-bonsai-#fff ${
+          isHidden ? "translate-x-full" : "translate-x-0"
+        } transition-all duration-300`}
+      >
+        <img src="/Images/sm-logo.svg" alt="Bonsai logo" className="w-10" />
+
+        <ul className="mt-16 text-bonsai-#4c4d5f text-[17px] font-proxima">
+          <li className="mt-3 pb-4 border-b">Product</li>
+          <li className="mt-3 pb-4 border-b">Templates</li>
+          <li className="mt-3 pb-4 border-b">Pricing</li>
+          <li className="mt-3 pb-4 border-b">Reviews</li>
+        </ul>
+
+        <span className="flex flex-col gap-3 mt-6">
+          <button className="py-2 border border-bonsai-#00b289 rounded-md text-bonsai-#00b289 text-sm font-proxima font-bold hover:bg-bonsai-#00b289 hover:text-bonsai-#fff">
+            LOG IN
+          </button>
+          <button className="py-2 rounded-md bg-bonsai-#00b289 text-bonsai-#fff text-sm font-proxima font-bold">
+            START FREE
+          </button>
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarSM;
